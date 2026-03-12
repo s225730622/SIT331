c16c3dfc63d552fa17dfa276123fed77af60135e
s225730622 <s225730622@deakin.edu.au>
2026-03-13 07:39:28 +1100

    Update Program.cs

--- Task 2.1P/Program.cs
+++ Task 2.1P/Program.cs
@@ -1,175 +1,81 @@
-using System;
-using System.Collections;
+// All the services for the web application are configured between lines 1 and 3
+using Robot.Api.Dtos;
 
-namespace SIT331_1_1P
+var builder = WebApplication.CreateBuilder(args); 
+
+var app = builder.Build();
+        
+// Create a list of robot commands
+List<CommandDto> commands = [
+    new (1, "LEFT"), 
+    new (2, "RIGHT"),
+    new (3, "PLACE"),
+    new (4, "MOVE"),
+];
+
+// Create a robot map
+List<MapDto> maps = [
+    new (1, 25, 5, 8)
+];
+
+// Browser Test, "Hello, World!"   >>   GET /
+app.MapGet("/", () => "Hello World!");
+
+// Retrieve all robot commands   >>   GET /robot-commands
+app.MapGet("/robot-commands", () => commands);
+
+// Retrieve all commands that move the robot   >>   GET /robot-commands/move
+app.MapGet("/robot-commands/move", () => commands.Where(command => command.Command == "MOVE"));
+
+// Retrieve a robot command by ID   >>   GET /robot-commands/{id}
+app.MapGet("/robot-commands/{id}", (int id) => commands.Find(command => command.CommandId == id))
+    .WithName("GetCommand");
+
+// Add a new command   >>   POST /robot-commands
+app.MapPost("/robot-commands", (CreateCommandDto newCommand) =>
 {
-    // Add an enumeration method for each direction the robot faces
-    public enum Direction
-    {
-        NORTH,
-        EAST,
-        SOUTH,
-        WEST
-    }
+    CommandDto command = new(
+        commands.Count + 1,
+        newCommand.Command
+    );
+    commands.Add(command);
 
-    // Create a Robot class which completes all required commands and implements all program constraints
-    public class Robot
-    {
+    return Results.CreatedAtRoute("GetCommand", new {id = command.CommandId}, command);
+});
 
-        public int X { get; set; }
-        public int Y { get; set; }
-        public Direction D { get; set; }
-        public int mapSize { get; } // Get the N size of map
-        public bool placed { get; private set; } // Ensure robot doesn't move until 'placed' equals true
+// Update an existing robot command   >>   PUT /robot-commands/{id}
+app.MapPut("/robot-commands/{id}", (int id, UpdateCommandDto updatedCommand) =>
+{
+    var index = commands.FindIndex(command => command.CommandId == id);
 
-        // Constructor for Robot class
-        public Robot(int mapsize)
-        {
-            mapSize = mapsize;
-            placed = false;
-        }
+    commands[index] = new CommandDto(
+        id,
+        updatedCommand.Command
+    );
 
-        // Place the robot using the x,y,d parameters
-        public void Place(int x, int y, Direction d)
-        {
-            X = x;
-            Y = y;
-            D = d;
-            placed = true;
-        }
+    return Results.NoContent();
+});
 
-        // Turn the robot
-        public void Turn(string turnDirection)
-        {
-            if (turnDirection == "Left")
-            {
-                D = (Direction)(((int)D + 3) % 4);
-            }
-            else if (turnDirection == "Right")
-            {
-                D = (Direction)(((int)D + 1) % 4);
-            }
-        }
-
-        // Move the robot one space depending on the direction it is facing
-        public void Move()
-        {
-            int newX = X;
-            int newY = Y;
-
-            switch (D)
-            {
-                case Direction.NORTH:
-                    newY++;
-                    break;
-                case Direction.EAST:
-                    newX++;
-                    break;
-                case Direction.SOUTH:
-                    newY--;
-                    break;
-                case Direction.WEST:
-                    newX--;
-                    break;
-            }
-
-            if (validPosition(newX, newY))
-            {
-                X = newX;
-                Y = newY;
-            }
-            else
-            {
-                Console.WriteLine("Invalid move! Robot is at the edge of the map and cannot fall off.");
-            }
-        }
-
-        // Ensure the robot is in a valid position
-        public bool validPosition(int x, int y)
-        {
-            return x >= 0 && x < mapSize && y >= 0 && y < mapSize;
-        }
-    }
-
-    // The Program class creates an instance of a robot and a map size N
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            // Put a do while in here??
-            Console.Write("Select a map size for the robot to move within: ");
-            int mapSize = int.Parse(Console.ReadLine());
-            var robot = new Robot(mapSize);
-            // Display an error message explaining map size constraints
-            if (mapSize > 100 | mapSize < 2)
-            {
-                Console.WriteLine("Invalid map size entered! Map size must be larger than 2 and smaller than 100.");
-                return;
-            }
-            // The below methods call all available robot commands
-            while (true)
-            {
-                Console.Write("Enter a command for the Robot: ");
-                string command = Console.ReadLine().Trim().ToUpper();
-                // If PLACE command is selected
-                if (command.StartsWith("PLACE"))
-                {
-                    string xVal = command.Split('(', ',')[1];
-                    string yVal = command.Split('(', ',')[2];
-                    string dVal = command.Split('(', ',', ')')[3];
-                    string[] values = command.Split(',');
-                    // Check the correct value formats were entered and place the robot if values are valid
-                    if (values.Length == 3 && (int.TryParse(xVal, out int x)) && (int.TryParse(yVal, out int y)) && Enum.TryParse(dVal, out Direction d))
-                    {
-                        robot.Place(x,y,d);
-                        Console.WriteLine($"Robot Placed.\nCurrent Position: ({robot.X}, {robot.Y}), Facing: {robot.D}");
-                    }
-                    // Display error message if input is invalid.
-                    else
-                    {
-                        Console.WriteLine("Invalid values entered for PLACE command. Please enter values in the format: PLACE(x,y,d)");
-                    }
-                }
-                // Once robot is placed, LEFT, RIGHT & MOVE selections can be selected as below
-                if (robot.placed)
-                {
-                    // If MOVE robot is selected
-                    if (command == "MOVE")
-                    {
-                        robot.Move();
-                        Console.WriteLine($"Current Position: ({robot.X}, {robot.Y}), Facing: {robot.D}");
-                    }
-                    // To turn robot LEFT
-                    else if (command == "LEFT")
-                    {
-                        robot.Turn("Left");
-                        Console.WriteLine($"Current Position: ({robot.X}, {robot.Y}), Facing: {robot.D}");
-                    }
-                    // To turn robot RIGHT
-                    else if (command == "RIGHT")
-                    {
-                        robot.Turn("Right");
-                        Console.WriteLine($"Current Position: ({robot.X}, {robot.Y}), Facing: {robot.D}");
-                    }
-                    // Display an invalid command error message if anything else was entered
-                    else if(command.StartsWith("PLACE") != true)
-                    {
-                        Console.WriteLine("Invalid Command!");
-                    }
-                }
-                // Error message is displayed if user tries to enter a command when robot is not yet placed
-                else
-                {
-                    Console.WriteLine("Robot must be placed first before it can complete any other commands.");
-                }
-            }
-        }
-    }
-}
+// Retrieve a robot map   >>   GET /robot-map
+app.MapGet("/robot-map", () => maps);    
 
 
+// Retrieve a coordinate in the form of x-y (e.g. 0-0, 3-5), return True if coordinate belongs to map and false otherwise   >>   GET /robot-map/{coordinate}
+app.MapGet("/robot-map/{coordinate}", (string coordinate) => maps.Any(map => map.Coordinate == coordinate));
 
+// Update an existing map with a new size   >>   PUT /robot-map
+app.MapPut("/robot-map", (UpdateMapDto updatedMap) => 
+{
+    var current = maps[0];
 
+    maps[0] = new MapDto(
+        current.MapId,
+        updatedMap.MapSize,
+        current.XVal,
+        current.YVal
+    );
 
+    return Results.NoContent();
+});
 
-
+app.Run();
