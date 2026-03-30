312c4a93f1b6b55e00f2af35c6a203cd44150c4b
s225730622 <s225730622@deakin.edu.au>
2026-03-31 02:06:02 +1100

    Update RobotCommand.cs

--- Task 3.2P/RobotCommand.cs
+++ Task 3.2P/RobotCommand.cs
@@ -2,22 +2,26 @@ namespace robot_controller_api;
 
 public class RobotCommand
 {
-    /// Implement <see cref="RobotCommand"> here following the task sheet requirements
+    // Add public properties for RobotCommand class
     public int Id { get; set; }
-    string Name { get; set; }
-    string? Description { get; }
-    bool IsMoveCommand { get; set; }
-    DateTime CreatedDate { get; set; }
-    DateTime ModifiedDate { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public bool IsMoveCommand { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
 
+    // Create constructor to initialize property on command creation
     public RobotCommand(
         int id, string name, bool isMoveCommand, DateTime createdDate, DateTime modifiedDate, string? description = null)
-        {
-            Id = id;
-            Name = name;
-            IsMoveCommand = isMoveCommand;
-            CreatedDate = createdDate;
-            ModifiedDate = modifiedDate;
-            Description = description;
-        }
+    {
+        // Initialize every property with parameters
+        Id = id;
+        Name = name;
+        IsMoveCommand = isMoveCommand;
+        CreatedDate = createdDate;
+        ModifiedDate = modifiedDate;
+        Description = description;
+    }
 }
+
+
